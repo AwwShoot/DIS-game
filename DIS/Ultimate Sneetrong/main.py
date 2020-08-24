a1959deff6bacfdd50b2092b7d4190e894027f68
AwwShoot <parker_luce@yahoo.com>
2020-08-24 09:00:17 -0400

    pygame

--- DIS/Ultimate Sneetrong/main.py
+++ DIS/Ultimate Sneetrong/main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
 
 
 def print_hi(name):
@@ -12,5 +13,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+screen = pygame.display.set_mode((640, 480))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
