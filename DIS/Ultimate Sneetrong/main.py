97b4afd11a4b89a097e296706fcde6537a91d610
AwwShoot <parker_luce@yahoo.com>
2020-08-28 10:02:00 -0400

    made some comments and a good(I think) screen size

--- DIS/Ultimate Sneetrong/main.py
+++ DIS/Ultimate Sneetrong/main.py
@@ -4,16 +4,22 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pygame
 
+"""
+click the green play button in the top right to run the code. 
+It will only run this specific file.
+All other code must be referenced and ran here.
+"""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+screen = pygame.display.set_mode((960, 560))
 
+"""
+Infinite loop keeps the script loaded so the screen is onscreen. 
+press CTRL+C or click the red square in pyCharm to force close the code.
+"""
+while True:
+    pass
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-screen = pygame.display.set_mode((640, 480))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# See Python help at https://docs.python.org/3/tutorial/
+# See PyGame help at https://www.pygame.org/docs/
