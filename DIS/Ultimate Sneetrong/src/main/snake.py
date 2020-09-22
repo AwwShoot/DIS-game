f03626a9ebc8368169c8400dfd1d99afb9713354
AwwShoot <parker_luce@yahoo.com>
2020-09-22 11:25:29 -0400

    grid fix

--- DIS/Ultimate Sneetrong/src/main/snake.py
+++ DIS/Ultimate Sneetrong/src/main/snake.py
@@ -14,7 +14,8 @@ class Snake():
         self.coordinates=coordinates
         self.collision_boxes=[]
         for i in coordinates:
-            self.collision_boxes.append(rect.Rect(i[0], i[1], 64, 64))
+            self.collision_boxes.append(rect.Rect(i[0]*64, i[1]*64, 64, 64))
+            # coordinates are based off a 64x64 pixel grid so each rect object will be instantiated at 64 pixels times the grid coordinate.
 
 
     """
@@ -22,13 +23,14 @@ class Snake():
     This function will modify the coordinates of the snake appropriately 
     """
     def move(self, direction):
+        # coordinates[2] represents the current head piece before a new head piece is made
         if direction=="up":
             log.write("moving up from: ")
             logwriter.write_coordinates(self.coordinates)
             self.coordinates.pop(0)
             self.collision_boxes.pop(0)
             self.coordinates.append([self.coordinates[2][0], self.coordinates[2][1] + 1])
-            self.collision_boxes.append(rect.Rect(self.coordinates[2][0], self.coordinates[2][1] + 1, 64, 64))
+            self.collision_boxes.append(rect.Rect(self.coordinates[2][0]*64, (self.coordinates[2][1] + 1)*64, 64, 64))
             logwriter.write_coordinates(self.coordinates)
 
         if direction=="down":
@@ -37,7 +39,7 @@ class Snake():
             self.coordinates.pop(0)
             self.collision_boxes.pop(0)
             self.coordinates.append([self.coordinates[2][0], self.coordinates[2][1] - 1])
-            self.collision_boxes.append(rect.Rect(self.coordinates[2][0], self.coordinates[2][1] - 1, 64, 64))
+            self.collision_boxes.append(rect.Rect(self.coordinates[2][0]*64, (self.coordinates[2][1] - 1)*64, 64, 64))
             logwriter.write_coordinates(self.coordinates)
         if direction=="left":
             log.write("moving left from: ")
@@ -45,7 +47,7 @@ class Snake():
             self.coordinates.pop(0)
             self.collision_boxes.pop(0)
             self.coordinates.append([self.coordinates[2][0]-1, self.coordinates[2][1]])
-            self.collision_boxes.append(rect.Rect(self.coordinates[2][0]-1, self.coordinates[2][1], 64, 64))
+            self.collision_boxes.append(rect.Rect((self.coordinates[2][0]-1)*64, self.coordinates[2][1]*64, 64, 64))
             logwriter.write_coordinates(self.coordinates)
         if direction=="right":
             log.write("moving right from: ")
@@ -53,7 +55,7 @@ class Snake():
             self.coordinates.pop(0)
             self.collision_boxes.pop(0)
             self.coordinates.append([self.coordinates[2][0]+1, self.coordinates[2][1]])
-            self.collision_boxes.append(rect.Rect(self.coordinates[2][0]+1, self.coordinates[2][1], 64, 64))
+            self.collision_boxes.append(rect.Rect((self.coordinates[2][0]+1)*64, self.coordinates[2][1]*64, 64, 64))
             logwriter.write_coordinates(self.coordinates)
 
 
